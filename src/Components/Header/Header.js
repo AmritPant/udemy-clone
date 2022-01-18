e820749894bb52d7995b34352e631bba155863b2
amrit <info.amritpant@gmail.com>
2022-01-18 13:45:41 +0545

    Header Added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.scss";
+
+import { LOGO__LINK } from "../../Helpers/config";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import LanguageIcon from "@mui/icons-material/Language";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo-box">
+        <img src={LOGO__LINK} alt="Logo" className="header__logo" />
+        <a href="#" className="header__link">
+          Categories
+        </a>
+      </div>
+      <div className="header__search-box">
+        <SearchOutlinedIcon fontSize="large" className="header__search-icon" />
+        <input
+          type="text"
+          className="header__input"
+          placeholder="Search for Anything"
+        />
+      </div>
+      <div className="header__nav">
+        <ShoppingCartOutlinedIcon fontSize="large" />
+        <button className="header__btn header__btn--unfilled"> Log in </button>
+        <button className="header__btn header__btn--filled"> SignUp </button>
+        <button className="header__btn header__btn--unfilled  lang-btn">
+          <LanguageIcon fontSize="large" />
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Helpers/config.js
@@ -0,0 +1,2 @@
+export const LOGO__LINK =
+  "https://www.udemy.com/staticx/udemy/images/v7/logo-udemy.svg";
